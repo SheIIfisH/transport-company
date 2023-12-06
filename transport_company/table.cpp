95b44c1cb1d793fe59e368e5c4d39b4ce238dc8e
Mirishkin Aleksandr <mirishkinaleksandr@gmail.com>
2023-12-06 14:43:49 +0300

    Add id to TableRow

--- transport_company/table.cpp
+++ transport_company/table.cpp
@@ -5,17 +5,17 @@ uint8_t Table::newRow(TableRow *p_row)
     return 0;
 }
 
-TableRow *Table::getRow(const uint8_t p_index)
+TableRow *Table::getRow(const uint16_t p_index)
 {
     return nullptr;
 }
 
-uint8_t Table::editRow(const uint8_t p_index, const TableRow *p_row)
+uint8_t Table::editRow(const uint16_t p_index, TableRow *p_row)
 {
     return 0;
 }
 
-uint8_t Table::deleteRow(const uint8_t p_index)
+uint8_t Table::deleteRow(const uint16_t p_index)
 {
     return 0;
 }
