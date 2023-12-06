20840479b767ebeb4739412be1fb3e99bdc4f165
Mirishkin Aleksandr <mirishkinaleksandr@gmail.com>
2023-12-06 10:39:38 +0300

    Add MainMenu class

---
+++ transport_company/mainmenu.cpp
@@ -0,0 +1,37 @@
+#include "mainmenu.h"
+
+MainMenu::MainMenu()
+{}
+
+uint8_t MainMenu::showPrimises()
+{
+    return 0;
+}
+
+uint8_t MainMenu::showEmployees()
+{
+    return 0;
+}
+
+uint8_t MainMenu::showTransport()
+{
+    return 0;
+}
+
+uint8_t MainMenu::showOrders()
+{
+    return 0;
+}
+
+uint8_t MainMenu::showExpenses()
+{
+    return 0;
+}
+
+uint8_t MainMenu::createReport()
+{
+    return 0;
+}
+
+
+
---
+++ transport_company/mainmenu.h
@@ -0,0 +1,25 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <stdint.h>
+#include "table.h"
+
+class MainMenu
+{
+private:
+    Table m_premisesTable;
+    Table m_employeeTable;
+    Table m_transportTable;
+    Table m_orderTable;
+    Table m_expenseTable;
+public:
+    MainMenu();
+    uint8_t showPrimises();
+    uint8_t showEmployees();
+    uint8_t showTransport();
+    uint8_t showOrders();
+    uint8_t showExpenses();
+    uint8_t createReport();
+};
+
+#endif // MAINMENU_H
