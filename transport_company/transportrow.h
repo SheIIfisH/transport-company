163cf58000ffb6fa2aad54adcae982eb8f561f67
Oleg Trukhnin <oleg.truhnin@gmail.com>
2023-12-07 22:33:38 +0300

    Add edit to TransportRow

--- transport_company/transportrow.h
+++ transport_company/transportrow.h
@@ -20,7 +20,8 @@ public:
                  std::string p_driver,
                  uint32_t p_payload,
                  TechStatus_t p_TechCond);
-    virtual uint8_t show() = 0;
+    virtual uint8_t show();
+    virtual uint8_t edit(TableRow * p_row);
     void setNumber(uint16_t p_number);
     uint16_t getNumber(){return m_number;}
     void setDriver(std::string p_driver);
