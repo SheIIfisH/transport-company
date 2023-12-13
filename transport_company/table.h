e5f4694cfcffbc66bfbc2f7d2b8800eb419137af
Maxim Fedotov 21312 <maxfm2003@mail.ru>
2023-12-13 20:08:24 +0300

    Add implementation of Table

--- transport_company/table.h
+++ transport_company/table.h
@@ -19,10 +19,11 @@ private:
     std::list<TableRow*> m_content;
 public:
     Table(){}
+    uint8_t showRows();
     uint8_t newRow(TableRow* p_row);
-    TableRow* getRow(const uint16_t p_index);
-    uint8_t editRow(const uint16_t p_index, TableRow* p_row);
-    uint8_t deleteRow(const uint16_t p_index);
+    TableRow* getRow(const uint16_t p_id);
+    uint8_t editRow(const uint16_t p_id, TableRow* p_row);
+    uint8_t deleteRow(const uint16_t p_id);
 };
 
 #endif // TABLE_H
